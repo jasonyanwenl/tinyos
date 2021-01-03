7eb1ed9be857bf50d4a5d46a92d9646e254671d8
Yanwen Lin <lyw1124278064@gmail.com>
2021-01-03 03:48:23 +0000

    remove 32-bit flags;

--- add_kernel.sh
+++ add_kernel.sh
@@ -12,16 +12,16 @@ nasm -I boot/include/ -o build/mbr.bin boot/mbr.S
 nasm -I boot/include/ -o build/loader.bin boot/loader.S
 
 # compile kernel
-gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -o build/main.o kernel/main.c
-gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -o build/interrupt.o kernel/interrupt.c
-gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -o build/init.o kernel/init.c
+gcc -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -o build/main.o kernel/main.c
+gcc -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -o build/interrupt.o kernel/interrupt.c
+gcc -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -o build/init.o kernel/init.c
 nasm -f elf -o build/kernel.o kernel/kernel.S
 
 # compile kernel lib
 nasm -f elf -o build/print.o lib/kernel/print.S
 
 # link kernel & lib
-ld -m elf_i386 -Ttext 0xc0001500 -e main -o \
+ld -Ttext 0xc0001500 -e main -o \
     build/kernel.bin build/main.o build/print.o build/interrupt.o build/init.o \
     build/kernel.o
 
